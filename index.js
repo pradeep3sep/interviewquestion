99181dd732fee7da8c4b88799452a68d96309319
pradeepvermagh <pradeepverma.gh@gmail.com>
2022-09-06 10:31:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//* Question 1
+// const numbers = [10,20,30,40,50,60]
+// const [,,,...newNumbers] = numbers
+// console.log(newNumbers);
+//?[40,50,60]
+
+//* Question 2
+// const details = {
+//     name : "Pradeep",
+//     age : 25,
+//     name : "Himanshu"
+// }
+// console.log(details);
+//? age: 25
+//?name: "Himanshu"
+
+//* Question 3
+// const name = "testify"
+// console.log(!typeof name === "object");
+// console.log(!typeof name === "string");
+// console.log(!typeof name); this is for hint
+
+//? false
+//? false
+
+//* Question 4
+// let a = 1
+// c= 2;
+// console.log(--c === a);
+//? true
+
+//* Question 5
+// let check = [1,2,3,4]
+// let hi = [,...check]
+// console.log(hi);
+
+//* Question 5
+//how to add "<h1>Hello World</h1>" in jsx
+// export default function App() {
+//     const htmlString = "<h1>Hello World</h1>";
+//     return <div dangerouslySetInnerHTML={{ __html: htmlString }}>
+//            </div>;
+//   }
+//? true
