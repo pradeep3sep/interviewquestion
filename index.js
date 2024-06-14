aa3263f1958aed02771414594cf95c614b660726
Pradeep Verma <119423109+pradeep3sep@users.noreply.github.com>
2024-06-14 13:27:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,6 @@
 //! https://www.thatjsdude.com/interview/js1.html#isPrime
 
 
-//* html 5 comes with semantic tags
-//* doctype in html, 
-//All HTML documents must start with a <!DOCTYPE> declaration. The declaration is not an HTML tag. It is an "information" to the browser about what document type to expect. In HTML 5, the declaration is simple: <!DOCTYPE html>
-
-//* Iframe in html : An inline frame is used to embed another document within the current HTML document.
-//* HTML Formatting Elements 
-// Formatting elements were designed to display special types of text:
-// <b> - Bold text
-// <strong> - Important text
-// <i> - Italic text
-// <em> - Emphasized text
-// <mark> - Marked text
-// <small> - Smaller text
-// <del> - Deleted text
-// <ins> - Inserted text
-// <sub> - Subscript text
-// <sup> - Superscript text
-
-//* Difference between <b> and <strong> Elements
-// The HTML <b> element defines bold text, without any extra importance. The HTML <strong> element defines text with strong importance. The content inside is typically displayed in bold.
-
-//*   semantic tag === A semantic element clearly describes its meaning to both the browser and the developer.
-
-//! -------------------------CSS ------------------------
-
-//* Box-Sizing :
-// The box-sizing property in CSS is used to control how the total width and height of an HTML element is calculated. It has two possible values: content-box and border-box.
-// The default value for box-sizing is content-box. This means that the width and height of an element are calculated based on the content of the element, excluding padding and border.
-// If you set the value of box-sizing to border-box, the width and height of an element are calculated based on the content, padding, and border of the element. This can be useful in situations where you want to set a specific width for an element that includes its padding and border, rather than having to calculate the total width manually.
-
-//* Pseudo-elements : befor and after
-//A CSS pseudo-element is a keyword added to a selector that lets you style a specific part of the selected element(s). For example, ::first-line can be used to change the font of the first line of a paragraph.
-
-//* Pseudo-classes/ Pseudo selector : hover,focus,active,link,first child,last child,nth child
-// A pseudo-class is used to define a special state of an element.
-
-//* What is flexbox
-// Flexbox is a layout module in CSS that provides a more efficient and flexible way to arrange and align elements in a container. The main idea behind Flexbox is that you can turn any container element into a flexible container,
-
-//* Position in css
-// position: static;
-// position: relative;
-// position: absolute;
-// position: fixed;
-// position: sticky;
-
-//* Screen meaning in media queries
-
-//* Advantage of CSS3 over CSS  === refer https://www.geeksforgeeks.org/advantage-of-css3-over-css/    and some are Responsiveness or Media queries,new color formats Colors, advanced Animations, etc
-
-//*   diff between visibility and d none  == visibility:hidden hides the element, but it still takes up space in the layout. display:none removes the element from the document. It does not take up any space.
-
-//* selectors in css ====  https://www.w3schools.com/css/css_selectors.asp
-
-//! -------------------------VANILLA JS ------------------------ 
-
-//* What is the difference between classical inheritance and prototypal inheritance?
-// Class Inheritance: instances inherit from classes (like a blueprint - a description of the class), and create sub-class relationships: hierarchical class taxonomies. Instances are typically instantiated via constructor functions with the new keyword. Class
-// inheritance may or may not use the class keyword from ES6.
-// Prototypal Inheritance: instances inherit directly from other objects. Instances are typically instantiated via factory functions
-// or Object.create(). Instances may be composed from many different objects, allowing for easy selective inheritance.
 
 
 
